74917b7dabe80e05c94429af2e0e68e892c1e4fa
nmq2111 <nooraq123@gmail.com>
2025-04-29 09:58:50 +0300

    UNO

--- Desk&Setup.js
+++ Desk&Setup.js
@@ -1,10 +1,14 @@
+//Desk & Setup functions
+//this functions is the main and the base of this game
+
+//variables in both functions
 const cardsContainer = document.querySelector('.cards-container')
-const allCards = Array.from(cardsContainer.querySelectorAll('.card'))
-const playerHand = []
-const computerHand = []
 
 function createDesk() {
+  // to grab cards from cards.html to the board.html
+  // SOMETHING NEW TO ME : DOMContentLoaded => It means "the page's structure (the DOM) is ready to work with"
   document.addEventListener('DOMContentLoaded', () => {
+    ;``
     fetch('Card.html')
       .then((response) => response.text())
       .then((data) => {
@@ -24,23 +28,23 @@ function createDesk() {
 }
 
 function RandomDesk() {
-  if (allCards.length < 7) {
-    return
-  }
+  const allCards = Array.from(cardsContainer.querySelectorAll('.card'))
+  const playerHand = []
+  const computerHand = []
 
   for (let i = 0; i < 7; i++) {
     let randomIndex = Math.floor(Math.random() * allCards.length)
     let selectedCard = allCards[randomIndex]
     playerHand.push(selectedCard)
-    allCards.splice(randomIndex, 1)
+    allCards.splice(randomIndex, 1) //!!!!!!!!!!!!!!!!
     cardsContainer.removeChild(selectedCard)
 
-    const computerArea = document.querySelector('.ComputerHand')
-    computerHand.forEach(() => {
-      const hiddenCard = document.createElement('div')
-      hiddenCard.classList.add('card', 'back')
-      computerArea.appendChild(hiddenCard)
-    })
+    // const computerArea = document.querySelector('.ComputerHand')
+    // computerHand.forEach(() => {
+    //   const hiddenCard = document.createElement('div')
+    //   hiddenCard.classList.add('card', 'back')
+    //   computerArea.appendChild(hiddenCard)
+    // })
   }
 
   const indexSlots = document.querySelectorAll('.index')
